95c4e70cf060685128a3a448eec19f23216ddbc4
Johnny Santos <149924300+Johnsnt@users.noreply.github.com>
2024-03-28 20:52:10 -0300

    versão estável

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,20 +2,8 @@ import React from 'react';
 import Image from 'next/image';
 import style from './Header.module.css';
 
-<<<<<<< HEAD
 
 export default function Header({ theme, changeTheme }) {
-=======
-export default function Header({theme, setTheme}) {
-
-    const light = '--lightMode'
-    const dark = '--darkMode'
-    const changeTheme = () => {
-        theme === light ? setTheme(dark) : setTheme(light);
-        console.log(true)
-    }
-
->>>>>>> 0d66051ee844093e200895ad4b7fa2f0ed9fd02c
     return (
         <header id={theme ? style.headerdark : style.header}>
             <div className={`${style.logo_box} section-limit`}>
@@ -23,11 +11,11 @@ export default function Header({theme, setTheme}) {
                     <Image src="/assets/logo.png" width={100} height={100} alt="logo" />
                     <h1>TWI<span>LIGHT</span></h1>
                 </div>
-<<<<<<< HEAD
+
                 <input onClick={changeTheme} type="checkbox" id={style.toggle} />
-=======
+
                 <input onClick={changeTheme} type="checkbox" id={style.toggle}/>
->>>>>>> 0d66051ee844093e200895ad4b7fa2f0ed9fd02c
+
             </div>
 
         </header>
